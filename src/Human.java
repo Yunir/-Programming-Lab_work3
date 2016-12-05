35cb44218909cd734479482b4e19e037f0bf753c
Yunir Salimzyanov <yunir.vw@gmail.com>
2016-12-05 03:45:55 +0300

    Overrided 3 methods of the Object Class, and refactored "alive" objects

--- src/Human.java
+++ src/Human.java
@@ -1,3 +1,5 @@
+import javax.lang.model.element.Name;
+
 public class Human implements IHuman {
     private String Name;
     APlace place;
@@ -6,18 +8,23 @@ public class Human implements IHuman {
         place = p;
     }
 
+    @Override
+    public boolean equals(Object obj) {
+        return place.getPlace()==obj;
+    }
+
     void nostalgia(Planets p) {
         if(p == Planets.Moon)
             System.out.println("\nСкучаю я по земным цветам - сказал " + Name);
     }
 
     boolean investigate(){
-        if (place.getPlace().equals("холм")) {
+        if (equals("холм")) {
             System.out.print("Вау, какой красивый двухэтажный дом с верандой!");
             System.out.println(" - сказал " + Name);
             return true;
         } else {
-            System.out.print("-Тут ничего интересного");
+            System.out.print("Тут ничего интересного");
             System.out.println(" - сказал " + Name);
             return false;
         }
--- src/Shorty.java
+++ src/Shorty.java
@@ -1,17 +1,29 @@
 public class Shorty extends Human {
     int height;
     int iq;
+    int magic_iq;
     AThing inHands;
+
+    @Override
+    public int hashCode() {
+        return 100+this.iq;
+    }
 
     Shorty(String n, int h, int intel, APlace p) {
         super(n, p);
         height = h;
         iq = intel;
+        magic_iq = this.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return super.getName() + " с iq " + this.iq;
     }
 
     void grab(AThing t){
         inHands = t;
-        System.out.println(getName() + " взял в руки объект " + inHands.getType());
+        System.out.println(this.toString() + " взял в руки объект " + inHands.getType());
     }
 
     void work() {
